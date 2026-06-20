4a71b28c7a704da034181cae18c6780afa1afdaf
Arjun1-cyber <arjunmalik524@gmail.com>
2026-06-20 13:27:02 +0530

    Added Firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyARArGSd3uBUJrJDHzrpQ6DjkkmW6avzmQ",
@@ -7,10 +8,11 @@ const firebaseConfig = {
   projectId: "studio-2826885822-a9563",
   storageBucket: "studio-2826885822-a9563.firebasestorage.app",
   messagingSenderId: "1038662747761",
-  appId: "1:1038662747761:web:73d0e6471165692f0cfdd0",
+  appId: "1:1038662747761:web:73d0e6471165692f0cfdd0"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
